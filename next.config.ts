573e57615127eacfeb08e216c1eecce1b8c29ff5
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-02-05 20:50:39 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // reactStrictMode: true,
+  poweredByHeader: false,
+  optimizeFonts: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+    APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
+    APP_API_URL: process.env.REACT_APP_API_URL,
+    APP_UPLOADS_URL: process.env.REACT_APP_UPLOADS_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination:`${process.env.REACT_APP_API_URL}/:path*`,
+      },
+      {
+        source: '/uploads/:path*',
+        destination: `${process.env.REACT_APP_UPLOADS_URL}/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
